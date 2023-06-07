814aaa6001e3272a66027ddeb7251a957602301a
NimaArnar <129725571+NimaArnar@users.noreply.github.com>
2023-06-07 21:42:16 +0330

    Updated to V1.3

--- database.py
+++ database.py
@@ -5,6 +5,7 @@ db = SqliteDatabase('./legolist.db')
 class Lego(Model):
     id = IntegerField(primary_key=True)
     price = FloatField(index=True)
+    name = CharField(max_length=255)
     url = CharField(max_length=255)
 
     class Meta:
@@ -22,17 +23,19 @@ with db:
         try:
             idl = int(input("Lego id: "))
             pl = float(input("Lego price: "))
+            nl = input("Lego name: ")
             urll = input("Lego url: ")
             # Check if the Lego object with the given ID already exists
             existing_lego = Lego.get_or_none(id=idl)
             if existing_lego:
                 # If it exists, update its price and URL
                 existing_lego.price = pl
+                existing_lego.name = nl
                 existing_lego.url = urll
                 existing_lego.save()
             else:
                 # If it doesn't exist, create a new Lego object
-                new_lego = Lego.create(id=idl, price=pl, url=urll)
+                new_lego = Lego.create(id=idl, price=pl, name=nl, url=urll)
                 new_lego.save()
         except Exception as e:
             print(f"Error: {e}")
