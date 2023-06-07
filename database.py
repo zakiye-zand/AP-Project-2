9b573ce48aae6edc3b6f8e5d1c79e6a49f30578f
NimaArnar <129725571+NimaArnar@users.noreply.github.com>
2023-06-07 17:55:15 +0330

    Updated to V1.1

--- database.py
+++ database.py
@@ -1,28 +1,32 @@
 from peewee import *
-LDB = SqliteDatabase('./legolist.db')
-class Lego(Model):
-    id = IntegerField(primary_key = True)
-    price = IntegerField(index = True)
-    url = CharField(max_length= 255)
-#    constraints = [SQL('UNIQUE (id)')]
-    class Meta:
-        database = LDB
-        table_name = 'Legos'
-        indexes = (id, True)
-LDB.connect()
-LDB.create_tables([Lego])
-LDB.close()
 
-n = int(input("Enter item count."))
-for i in range(n):
-    LDB.connect()
-    idl = int(input("lego id"))
-    pl = float(input("lego price"))
-    urll = input("lego url")
-    new_lego = Lego.create(id = idl, price = pl, url = urll)
-    new_lego.save()
-    LDB.close()
-print(LDB)
-p = Lego.select()
-for i in p:
-    print(i.url)
+db = SqliteDatabase('./legolist.db')
+
+class Lego(Model):
+    id = IntegerField(primary_key=True)
+    price = FloatField(index=True)
+    url = CharField(max_length=255)
+
+    class Meta:
+        database = db
+        table_name = 'legos'
+
+db.connect()
+db.create_tables([Lego])
+db.close()
+
+n = int(input("Enter item count: "))
+with db:
+    for i in range(n):
+        try:
+            idl = int(input("Lego id: "))
+            pl = float(input("Lego price: "))
+            urll = input("Lego url: ")
+            new_lego = Lego.create(id=idl, price=pl, url=urll)
+            new_lego.save()
+        except Exception as e:
+            print(f"Error: {e}")
+
+with db:
+    legos = [lego.url for lego in Lego.select()]
+    print(legos)
