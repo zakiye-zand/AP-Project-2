129001e665333ace95153c2a32a600da4d717cd1
NimaArnar <129725571+NimaArnar@users.noreply.github.com>
2023-06-09 20:38:11 +0330

    Update database.py

--- database.py
+++ database.py
@@ -13,33 +13,5 @@ class Lego(Model):
         table_name = 'legos'
 
 db.connect()
-db.create_tables([Lego])
+db.create_tables([Lego]
 db.close()
-
-n = int(input("Enter item count: "))
-
-with db:
-    for i in range(n):
-        try:
-            idl = int(input("Lego id: "))
-            pl = float(input("Lego price: "))
-            nl = input("Lego name: ")
-            urll = input("Lego url: ")
-            # Check if the Lego object with the given ID already exists
-            existing_lego = Lego.get_or_none(id=idl)
-            if existing_lego:
-                # If it exists, update its price and URL
-                existing_lego.price = pl
-                existing_lego.name = nl
-                existing_lego.url = urll
-                existing_lego.save()
-            else:
-                # If it doesn't exist, create a new Lego object
-                new_lego = Lego.create(id=idl, price=pl, name=nl, url=urll)
-                new_lego.save()
-        except Exception as e:
-            print(f"Error: {e}")
-
-with db:
-    legos = [lego.url for lego in Lego.select()]
-    print(legos)
